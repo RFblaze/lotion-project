a3bad92444116192426104e0ba6f6e0303b3a501
Angelo Troncone <angelotroncone@gmail.com>
2023-02-28 20:30:53 -0700

    Made top bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 function App() {
-  return <h1>Lotion</h1>;
+  return (
+  <>
+  <div><button id="menu">&#9776;</button></div>
+  <div><h1 class ="title">Lotion</h1></div>
+  <div><h5 class = "title">Like Notion but worse</h5></div>
+  </>
+  );
 }
 
 export default App;
