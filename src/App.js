0d524bb2730e4e064eb67b9c1231c3780e56f691
Angelo Troncone <angelotroncone@gmail.com>
2023-03-04 13:11:38 -0700

    Add note button adds note divs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import React, {useState} from "react"
+
 function App() {
+  const [num, setNum] = useState(0);
+  
+  
   return (
   <>
-  <div><button id="menu">&#9776;</button></div>
-  <div><h1 class ="title">Lotion</h1></div>
-  <div><h5 class = "title">Like Notion but worse</h5></div>
+  <div id="top">
+    <div><button id="menu">&#9776;</button></div>
+    <div><h1 class ="title">Lotion</h1></div>
+    <div><h5 class = "title">Like Notion but worse</h5></div>
+  </div>
+  <div id="container">
+    <div id="notes-section">
+      <div id="header">
+        <h2>Notes</h2>
+        
+        <button onClick={ () => {
+          
+          const newNote = document.createElement("div")
+          // newNote.addEventListener("click")
+          const noteTitle = document.createElement("h3")
+          noteTitle.append(`New Note ${num}`)
+          setNum(num + 1)
+          newNote.append(noteTitle)
+          const notes = document.getElementById("note-list")
+          notes.appendChild(newNote)
+        } }>+</button>
+      </div>
+      <div id="note-list">
+      </div>
+    </div>
+    <div id="writing-section">
+      <h1>Select a note, or create a new one</h1>
+    </div>
+  </div>
+  
   </>
   );
 }
